54343ece73bcd07fc6d162e8daa616173e8b2f05
Stephm23 <Stephanie.mboya@strathmore.edu>
2020-08-28 21:30:44 +0300

    Freelancer Sign Up

---
+++ app/src/main/java/com/example/freelancer/FreelancerSignUp.java
@@ -0,0 +1,14 @@
+package com.example.freelancer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreelancerSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_freelancer_sign_up);
+    }
+}
